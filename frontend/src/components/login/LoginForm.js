b13b4592ea06196d8af90a1d2dbb364eed7c9db9
Madelane <madelanedaz@gmail.com>
2023-06-01 12:15:28 +0100

    changed email to username for logging in

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
 const LoginForm = ({ navigate }) => {
-    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
   return (
     <div className='login-form'>
     <form>
-    <input placeholder='Email' id="email" type='text' value={ email }/>
+    <input placeholder='Username' id="username" type='text' value={ username }/>
     <input placeholder='Password' id="password" type='password' value={ password }/>
     <input role='submit-button' id='submit' type="submit" value="Submit" />
   </form>
