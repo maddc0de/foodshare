82ef499d6dfe0828b5d5b27a555982b7f824fe51
Robin Vuyyuru <robinvuy@MASTUDEMB1VQ6L4.lan>
2023-06-01 14:37:02 +0100

    setting up signup form use state

--- frontend/src/Donator/SignupForm.js
+++ frontend/src/Donator/SignupForm.js
@@ -1,4 +1,24 @@
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 
 
-const signup
+const SignUpForm = ({ navigate }) => {
+
+  const [name, SetUserName] = useState("");
+  const [email, SetEmail] = useState("");
+  const [description, SetDescription] = useState("");
+  const [location, SetUserLocation] = useState("");
+  const [Username, SetUserUsername] = useState("");
+  const [password, SetUserPassword] = useState("");
+// const [picture, SetUserPicture] = useState("");
+
+  const handleSubmit = async (event) => { 
+    event.preventDefault();
+
+  }
+
+
+}
+
+
+
+export default SignUpForm;
