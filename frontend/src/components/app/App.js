04128f52e69f5885e934db160242aa4c43b48f8c
Manuela Iacobovici <miacobovici@yahoo.co.uk>
2023-06-02 11:57:21 +0100

    added signup form for collector

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import LoginForm from "../login/LoginForm";
+import SignUpForm from "../Collector/SignUpForm";
 import React from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
+      <Route path="/signupcollector" element={<SignUpForm navigate={useNavigate()} />} />
     </Routes>
   );
 };
