109fe4fbfaf7ae619f6becc98d7bd44f341c7ffc
bookloverbabe <solmaz@hotmail.co.uk>
2023-06-02 14:35:55 +0100

    Deleted read me branch

---
+++ frontend/src/components/Donator/SignupForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+
+const SignUpForm = ({ navigate }) => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+// const [picture, SetUserPicture] = useState("");
+
+  const handleSubmit = async (event) => { 
+    event.preventDefault();
+
+
+    const requestBody = JSON.stringify({
+      name: name,
+      email: email,
+      description: description,
+      location: location,
+      username: username,
+      password: password,
+    })
+
+    fetch( '/donators', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: requestBody,
+    })
+      .then(response => {  
+        // come back later, and re-make the route
+        if(response.status === 201) {
+        navigate('/login')
+        // setWelcomeMessage(`Welcome ${email}!`)
+      } else {
+        navigate('/signup')
+      }
+    });
+}
+
+    const handleNameChange = (event) => {
+      setName(event.target.value)
+    }
+  
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value)
+    }
+
+    const handleDescriptionChange = (event) => {
+      setDescription(event.target.value)
+    }
+
+    const handleLocationChange = (event) => {
+      setLocation(event.target.value)
+    }
+
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>{"hi"}</h2>
+            <input placeholder="name" id="name" type='text' value={ name } onChange={handleNameChange} />
+            <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
+            <input placeholder="description" id="description" type='text' value={ description } onChange={handleDescriptionChange} />
+            <input placeholder="location" id="location" type='text' value={ location } onChange={handleLocationChange} />
+            <input placeholder="username" id="username" type='text' value={ username } onChange={handleUsernameChange} />
+            <input placeholder="password" id="password" type='text' value={ password } onChange={handlePasswordChange} />
+          <input id='submit' type="submit" value="Submit" />
+        </form>
+      );
+    
+}
+
+
+
+
+
+export default SignUpForm;
--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginForm from "../login/LoginForm";
 import SignUpForm from "../Collector/SignUpForm";
+import SignUpForm from "../Donator/SignupForm";
 import React from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
       <Route path="/signupcollector" element={<SignUpForm navigate={useNavigate()} />} />
+      <Route path="/signupdonator" element={<SignUpForm navigate={useNavigate()} />} />
     </Routes>
   );
 };
