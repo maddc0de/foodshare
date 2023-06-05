1966c70eccdc41ec40506bc87cb19652f01228c9
bookloverbabe <solmaz@hotmail.co.uk>
2023-06-05 16:40:28 +0100

    Fixed app in frontend merge conflict

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-import LoginForm from "../login/LoginForm";
+import LoginCollector from "../Collector/LoginCollector";
+import LoginDonator from "../Donator/LoginDonator";
 import SignUpForm from "../Collector/SignUpForm";
-<<<<<<< HEAD
-import SignUpForm from "../Donator/SignupForm";
-=======
 import DonatorSignUpForm from '../Donator/SignUpForm';
->>>>>>> 252c83bcb030c06a319b71a8280e586c5b4f2901
+import DonatorFeed from '../donatorFeed/DonatorFeed';
 import React from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
-      <Route path="/signupcollector" element={<SignUpForm navigate={useNavigate()} />} />
-<<<<<<< HEAD
-      <Route path="/signupdonator" element={<SignUpForm navigate={useNavigate()} />} />
-=======
-      <Route path="/signupdonator" element={<DonatorSignUpForm navigate={useNavigate()} />} />
->>>>>>> 252c83bcb030c06a319b71a8280e586c5b4f2901
+      <Route path="/signup/collector" element={<SignUpForm navigate={useNavigate()} />} />
+      <Route path="/signup/donator" element={<DonatorSignUpForm navigate={useNavigate()} />} />
+      <Route path="/login/collector" element={<LoginCollector navigate={useNavigate()} />} />
+      <Route path="/login/donator" element={<LoginDonator navigate={useNavigate()} />} />
+      <Route path="/foodhero/:id" element={<DonatorFeed navigate={useNavigate()} />} />
     </Routes>
   );
 };
