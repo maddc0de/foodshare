bc47d277fc7c5ab99e416d1c264faf8575b29ca9
Madelane <madelanedaz@gmail.com>
2023-06-05 18:10:40 +0100

    added autogenerate code field for a donation

--- backend/models/donation.js
+++ backend/models/donation.js
@@ -4,7 +4,18 @@ const DonationSchema = new mongoose.Schema({
   food_heroes_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   description: { type: String, required: true },
   status: { type: String, default: "available" },
+  code: { type: String, default: generateCode, unique: true }
 });
+
+function generateCode() {
+  const digits = "0123456789";
+  let code = "";
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = Math.floor(Math.random() * digits.length);
+    code += digits[randomIndex];
+  }
+  return code;
+}
 
 const Donation = mongoose.model("Donation", DonationSchema);
 
