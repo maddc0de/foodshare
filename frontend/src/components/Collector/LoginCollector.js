6a2bda09150b65cce32fa5830ca77d50bef409d7
Arya Sunildath <aryadath@gmail.com>
2023-06-07 13:37:23 +0100

    Added food rescue default picture to login collector

--- frontend/src/components/Collector/LoginCollector.js
+++ frontend/src/components/Collector/LoginCollector.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import Navbar from "../navbar/Navbar";
 import LoginForm from "../login/LoginForm";
+import foodRescueDefault from "../../../public/food_rescue.png"; // Import the default Food Rescuer profile picture
+
 
 const LoginCollector = ({ navigate }) => {
  
   const [owner, setOwner] = useState("Food Rescuer");
-  // const [picture, SetUserPicture] = useState("");
+  const [picture, setPicture] = useState(foodRescueDefault);
+
 
   
   return (
     <>
-      <Navbar owner={owner}></Navbar>
+      <Navbar owner={owner} picture={picture}></Navbar> {/* Pass the profile picture as a prop to the Navbar component */}
 
-      <LoginForm owner={owner} navigate={navigate}></LoginForm>
+      <LoginForm owner={owner} picture={picture} navigate={navigate}></LoginForm> {/* Pass the profile picture as a prop to the LoginForm component */}
     </>
   );
 };
